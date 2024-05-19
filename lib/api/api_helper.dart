24b7b3d9cde413896de8af15d3d9f066e5b18d54
unknown <toannguyen230397@gmail.com>
2024-05-19 22:11:59 +0700

    change app name, change app icon

--- lib/api/api_helper.dart
+++ lib/api/api_helper.dart
@@ -1,27 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 class ChatAPI {
-  final String apiURL = 'http://192.168.1.4:3000/';
-
-  // Future<List<Map<String, dynamic>>> sendRequest(String msg, List history) async {
-  //   final response = await http.post(
-  //     Uri.parse('${apiURL}testAPI'),
-  //     headers: <String, String>{
-  //       'Content-Type': 'application/json; charset=UTF-8',
-  //     },
-  //     body: jsonEncode({
-  //       'msg': msg,
-  //       'history': history
-  //     }),
-  //   );
-
-  //   if (response.statusCode == 200) {
-  //     final List<Map<String, dynamic>> responseData = json.decode(response.body);
-  //     return responseData;
-  //   } else {
-  //     throw Exception('Failed to post data');
-  //   }
-  // }
+  final String apiURL = 'https://chatboxai-backend.onrender.com/';
 
   Future<Map<String, dynamic>> sendRequest(String msg, List history) async {
     final response = await http.post(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:chat_box_ai/bloc/chat/chat_bloc.dart';
 import 'package:chat_box_ai/bloc/chat_room/chat_room_bloc.dart';
 import 'package:chat_box_ai/screen/home.dart';
-import 'package:chat_box_ai/screen/test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
@@ -25,14 +24,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: const Test(),
-//       );
-//   }
-// }
